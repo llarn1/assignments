7755c3472fa8cc255be011fdbb179d7df90075a0
llarn1 <117578193+llarn1@users.noreply.github.com>
2024-09-06 13:57:56 +0300

    Create FullNameDisplay.cpp

---
+++ FullNameDisplay.cpp
@@ -0,0 +1,11 @@
+//cpp program to output the user's name 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string FirstName, LastName;
+    cin>>FirstName>>LastName;
+    cout<<FirstName<<" "<<LastName<<endl;
+    return 0;
+}
