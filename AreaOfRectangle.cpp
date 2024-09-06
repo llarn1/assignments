3be1308a884766591319f7ed1227c1f943109978
llarn1 <117578193+llarn1@users.noreply.github.com>
2024-09-06 15:30:46 +0300

    Create AreaOfRectangle.cpp

---
+++ AreaOfRectangle.cpp
@@ -0,0 +1,16 @@
+// cpp program to calculate the area of a rectangle.
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // take input
+    float hight, width;
+    cout<<"Enter the hight: ";
+    cin>>hight;
+    cout<<"Enter the width: ";
+    cin>>width;
+    // print the area
+    cout<<"The Area Of This Rectangle Equals To: "<<hight * width<<" metar squared."<<endl;
+    return 0;
+}
