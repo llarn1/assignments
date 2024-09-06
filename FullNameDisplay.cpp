1272a18681367b6597465d29624e1d92a6b1164c
llarn1 <117578193+llarn1@users.noreply.github.com>
2024-09-06 15:29:09 +0300

    Update FullNameDisplay.cpp

--- FullNameDisplay.cpp
+++ FullNameDisplay.cpp
@@ -1,11 +1,13 @@
-//cpp program to output the user's name 
+//cpp program to output the user's name.
 #include <iostream>
 using namespace std;
 
 int main ()
 {
+    // take input
     string FirstName, LastName;
     cin>>FirstName>>LastName;
+    
     cout<<FirstName<<" "<<LastName<<endl;
     return 0;
 }
