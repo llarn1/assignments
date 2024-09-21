0d3e795c82275a9522898a6fbc3a1f8a064cfbbb
rahaf-fathi <117578193+rahaf-fathi@users.noreply.github.com>
2024-09-21 23:28:46 +0300

    Update zeroHundred.cpp

--- week3/zeroHundred.cpp
+++ week3/zeroHundred.cpp
@@ -6,8 +6,8 @@ int main(void)
     float val;
     cout<<"Enter your number: ";
     cin>>val;
-    if (val >= 0 && val <= 100) cout<<"your number is between zero and one-hundred inclusive\n";
-    else cout<<"your provided number is not between zero and one-hundred\n";
+    if (val >= 0 && val <= 100) cout<<"your number is between 0 and 100 inclusive.\n";
+    else cout<<"your provided number is not between 0 and 100.\n";
     return 0;
 }
 
@@ -16,5 +16,5 @@ int main(void)
     input:
     Enter your number: 0.5
     output:
-    your number is between zero and one-hundred inclusive
+    your number is between 0 and 100 inclusive.
 */
