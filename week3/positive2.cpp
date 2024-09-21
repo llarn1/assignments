9bcf15b1e915ee03dbbddfb6c400e58b77c7ef22
rahaf-fathi <117578193+rahaf-fathi@users.noreply.github.com>
2024-09-21 22:27:45 +0300

    Create positive2.cpp

---
+++ week3/positive2.cpp
@@ -0,0 +1,14 @@
+// C++ program that prints positive if the provided numbers are positive.
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int x, y;
+    cout<<"enter your first and second values: ";
+    cin>>x>>y;
+
+    // taking zero as neither positive nor negative
+    if (x > 0 && y > 0) cout<<"Both Positive";
+    return 0;
+}
+
