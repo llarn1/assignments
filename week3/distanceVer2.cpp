e2e06cfa712a9cc046f2305591f4f05154e36ecb
rahaf-fathi <117578193+rahaf-fathi@users.noreply.github.com>
2024-09-22 01:27:27 +0300

    Create distanceVer2.cpp

---
+++ week3/distanceVer2.cpp
@@ -0,0 +1,21 @@
+// C++ program to calculate the distnace left to get a certain service.
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int d;
+    cout<<"Enter the miles traveled: ";
+    cin>>d;
+    // assuming at exactly 10,000 km the driver will take the minor service with zero distance left.
+    if (d > 10000)
+    {
+        int left = 20000 - d;
+        cout<<"Type: major\nDistance Left: "<<left<<"\n";
+    }
+    else
+    {
+        int left = 10000 - d;
+        cout<<"Type: minor\nDistance Left: "<<left<<"\n";
+    }
+    return 0;
+}
